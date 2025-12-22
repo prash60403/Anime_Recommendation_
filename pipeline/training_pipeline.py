59022318cc8613e4686847fdcdcca6f3b87a5b89
Prashanth HV <prashanthhv2003@gmail.com>
2025-12-22 15:18:05 +0530

    cimmit

---
+++ pipeline/training_pipeline.py
@@ -0,0 +1,16 @@
+from utils.Common_functions import read_yaml 
+from config.paths_config import *
+
+from src.data_processing import DataProcessor
+from src.model_training import ModelTraining
+
+
+
+if __name__=="__main__":
+
+    data_processor = DataProcessor(ANIMELIST_CSV,PROCESSED_DIR)
+    data_processor.run()
+
+    model_trainer = ModelTraining(PROCESSED_DIR)
+    model_trainer.train_model()
+
